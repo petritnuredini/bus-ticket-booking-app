b3ee0e0cc3c20c84c381d3a80919cb5d0cf2818a
TringaN <nevzatitringa@gmail.com>
2025-08-30 14:27:16 +0200

    added baseUrl

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
+  baseURL: "http://localhost:3001", // kjo është shumë e rëndësishme
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
